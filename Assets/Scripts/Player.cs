53eedfd5acf94f4357456f1a5e2135ebb8106403
Onur Kalkan <onurkalkanv84@hotmail.com>
2024-03-27 18:55:33 +0300

    UI_Code_Part_I

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Move move;
+    UIManager uimanager;
+    ScoreManager scoremanager;
+    float uiOpeningTime = 0.75f;
+
+    private void Awake()
     {
-        
+        move = GetComponent<Move>();
+        uimanager = GameObject.Find("GameManager").GetComponent<UIManager>();
+        scoremanager = GameObject.Find("GameManager").GetComponent<ScoreManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Finish"))
+        {
+            //move.AnimPlay("Celebrate");
+            move.speed = 0;
+            Invoke(nameof(WinCondition), uiOpeningTime);
+        }
+        if (other.CompareTag("Collectible"))
+        {
+            Destroy(other.gameObject);
+            //other.gameObject.SetActive(false);
+            scoremanager.UpdateStarScore();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            move.AnimPlay("Die");
+            move.speed = 0;
+            Invoke(nameof(FailCondition), uiOpeningTime);
+        }
+    }
+
+    void FailCondition()
+    {        
+        uimanager.FailPanel();
+    }
+
+    void WinCondition()
+    {
+        uimanager.WinPanel();
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,79 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject startPanel, inGamePanel, endPanel, winPanel, failPanel;
+    public TextMeshProUGUI starCountText;
+    GameObject player;
+
+    private void Awake()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OpenPanel(GameObject panelObject, GameObject secondPanel)
     {
-        
+        startPanel.SetActive(false);
+        inGamePanel.SetActive(false);
+        endPanel.SetActive(false);
+        winPanel.SetActive(false);
+        failPanel.SetActive(false);
+        panelObject.SetActive(true);
+        if (secondPanel != null)
+            secondPanel.SetActive(true);
     }
+
+    private void Start()
+    {
+        OpenPanel(startPanel, null);
+    }
+
+    public void FailPanel()
+    {
+        OpenPanel(failPanel, endPanel);
+    }
+
+    public void WinPanel()
+    {
+        OpenPanel(winPanel, endPanel);
+    }
+
+    #region Button Functions
+    public void TapToStart()
+    {
+        startPanel.SetActive(false);
+        inGamePanel.SetActive(true);
+        player.GetComponent<Move>().speed = 10;
+        player.GetComponent<Move>().AnimPlay("Run");
+    }
+
+    public void NextLevel()
+    {        
+        int currentLevel = PlayerPrefs.GetInt("CurrentLevel", 1);//levelim 7ydi integera 7 atadim        
+        /////
+        if(currentLevel < 200)
+        {
+            PlayerPrefs.SetInt("CurrentLevel", currentLevel + 1);//local datada CurrentLevel degerini 1 arttırdım 8 oldu
+            //SceneManager.LoadScene(currentLevel);//private currentLevel indexine sahip sahneyi cagirdim yani index no 7 levelini cagirdim
+            ////////////////////
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//sonraki sahnenin index numarasina gore leveli cagiriyorum
+        }
+        else if(currentLevel >= 200)
+        {
+            PlayerPrefs.SetInt("CurrentLevel", 0);//200uncu levele gelince datayi sifirla
+            SceneManager.LoadScene(0);//index no 0'i cagir
+        }
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//index numarasi bu olan, oldugum/kaybettigim sahneyi tekrar baslat
+    }
+    #endregion
+
 }
